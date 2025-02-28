580e82246d34a682343cf4aff012e7db3e07cf46
adanguerra1990 <aj90guerra@gmail.com>
2025-02-28 11:18:23 -0400

    Agregar funcionalidad de inicio de sesión y manejo de errores en la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,51 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LginForm'
+import Notify from './components/Notify'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState('')
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
+
+  const logout = () => {
+    setToken(null)
+    localStorage.removeItem('library-user-token')
+    setPage('authors')
+  }
+
+  const notify = message => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
 
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        {token && <button onClick={() => setPage('add')}>add book</button>}
+        {token ? (
+          <button onClick={logout}>logout</button>
+        ) : (
+          <button onClick={() => setPage('login')}>login</button>
+        )}
       </div>
+
+      <Notify errorMessage={errorMessage} />
 
       <Authors show={page === 'authors'} />
 
       <Books show={page === 'books'} />
 
       <NewBook show={page === 'add'} />
+
+      {page === 'login' && (
+        <LoginForm setToken={setToken} setPage={setPage} setError={notify} />
+      )}
     </div>
   )
 }
